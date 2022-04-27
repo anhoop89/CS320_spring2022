ed7801517b7a500aacd18278772b8aa6cbefcaed
Anh Ho <anhho@pdx.edu>
2022-04-26 23:56:21 -0700

    ass3

--- cs-320-assignment-3-main/src/Grammar/Lexer.ts
+++ cs-320-assignment-3-main/src/Grammar/Lexer.ts
@@ -4,6 +4,7 @@ import { compileLexer } from "../Library/Parsing";
 const lexingRules: Rules = {
   _: /[ \t]+/,
   hex:/-?\$+[0-9A-Fa-f]+/,
+  char:/\'+\\*?[.|0-9|A-Za-z|\\|\']+\'/,
   float: /-?\d+(?:\.\d*)?/,
   name: /[A-Za-z]\w*/,
   plus: /\+/,
